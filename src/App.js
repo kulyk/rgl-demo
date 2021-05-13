2bcb76ca33a437f2e49d9246011e41e2d6739a16
Anton Kulyk <kuliks.anton@gmail.com>
2021-05-13 14:20:08 +0300

    Fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useMemo, useState } from "react";
 import RGL, { WidthProvider } from "react-grid-layout";
 import ControlPanel from "./ControlPanel";
-import { getGridBackground, randomInteger } from "./utils";
 import "react-grid-layout/css/styles.css";
 import "react-resizable/css/styles.css";
 import "./styles.css";
@@ -10,29 +9,20 @@ const CARDS_COUNT = 10;
 const ASPECT_RATIO = 4 / 3;
 const MIN_ROW_HEIGHT = 60;
 
-function Grid({ isDraggable, margin, width, cols, ...props }) {
+function randomInteger(min, max) {
+  return parseInt(Math.random() * (max - min) + min);
+}
+
+function Grid(props) {
+  const { margin, width, cols } = props;
   const rowHeight = Math.max(
     Math.floor(width / cols / ASPECT_RATIO),
     MIN_ROW_HEIGHT
   );
 
   return (
-    <div
-      className="Grid"
-      style={{
-        backgroundImage: isDraggable
-          ? getGridBackground({ color: "#fafafa", margin, rowHeight, ...props })
-          : "",
-      }}
-    >
-      <RGL
-        cols={cols}
-        isDraggable={isDraggable}
-        width={width}
-        rowHeight={rowHeight}
-        margin={[margin, margin]}
-        {...props}
-      />
+    <div className="Grid">
+      <RGL rowHeight={rowHeight} {...props} margin={[margin, margin]} />
     </div>
   );
 }
@@ -43,7 +33,6 @@ function App() {
   const [{ margin, ...state }, setGridState] = useState({
     cols: 18,
     margin: 6,
-    rowHeight: 150,
     isDraggable: true,
     isResizable: true,
     isBounded: false,
@@ -55,7 +44,7 @@ function App() {
     return new Array(CARDS_COUNT).fill(undefined).map((val, i) => {
       const x = (i * 3) % 18;
       const w = 3;
-      const h = randomInteger(1, 3);
+      const h = randomInteger(2, 5);
       return (
         <div
           key={i}
